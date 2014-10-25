28f5be45e09e9007e3be9b54f202971c285ca17b
Matthew O. Smith <matt@m0smith.com>
2014-10-24 19:05:29 -0600

    Added parents-of, generation, names

--- src/topoged/pedigree/core.clj
+++ src/topoged/pedigree/core.clj
@@ -31,10 +31,47 @@
   (->> (gedcom-seq lines)
        (reduce process-record result)))
 
-(defn gedcom-file [f]
+(defn gedcom-file 
+  "Read a GEDCOM and output the PedigreeModel"
+  [f]
   (with-open [rdr (reader f)]
     (read-lines {} (line-seq rdr))))
 
-;;  (ggg  (line-seq (reader "test-resources/TiberiusClaudiusCaesarAugustusGermanicusClaudiusEmperorofRome.ged"))))
+(defn names [model]
+  (map (juxt :name :id) (vals model)))
+
+
+(defn parents-of [model id]
+  (condp = id
+    nil [:unknown :unknown]
+    :unknown [:unknown :unknown]
+    (let [rtnval (if id
+                   (->> (model id)
+                        :parents))]
+      (or rtnval [:unknown :unknown]))))
+
+(defn bf-tree-seq [branch? children tree]
+  ((fn step [queue]
+     ;(println "<<" (seq queue) ">>")
+
+     (lazy-seq
+      (when (seq queue)
+        (let [node (peek queue)]
+          ;(println "node:"  node "branch?" (branch? node))
+          (if (branch? node)
+            (cons node (step (into (pop queue) (children node))))
+            (cons node (step (pop queue))))))))
+   (conj clojure.lang.PersistentQueue/EMPTY tree)))
+
+(defn generation [ns]
+  (concat (map (partial str "F") ns) (map (partial str "M") ns)))
+  
+
+; (def model (gedcom-file "test-resources/TiberiusClaudiusCaesarAugustusGermanicusClaudiusEmperorofRome.ged"))
+; (defn xx [x] (map vector (mapcat identity (take 8 (iterate generation [""]))) (bf-tree-seq (partial parents-of model) (partial parents-of model) x)))
+; (filter #(!= :unknown (last (last %))) (map xx (map second (names model))))
+
+
+;  (ggg  (line-seq (reader "test-resources/TiberiusClaudiusCaesarAugustusGermanicusClaudiusEmperorofRome.ged"))))
 
 
