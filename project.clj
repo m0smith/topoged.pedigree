23a4318f0bd2adb70ca08c23eb3a2bce56c4221f
Matthew O. Smith <matt@m0smith.com>
2014-10-27 22:50:03 -0600

    OM/React hello world

--- project.clj
+++ project.clj
@@ -3,10 +3,22 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/clojurescript "0.0-2322"]
+                 [topoged/gedcom "0.1.0"]
+                 [om "0.8.0-alpha1"]]
+  :plugins [[lein-cljsbuild "1.0.3"]]
+  :cljsbuild {
+              :builds [{:id "dev"
+                        :source-paths ["src"]
+                        :compiler {
+                                   :output-to "main.js"
+                                   :output-dir "out"
+                                   :optimizations :none
+                                   :source-map true}}]}
   :profiles { :dev 
              { :dependencies [[org.clojure/core.async "0.1.346.0-17112a-alpha"]
-                              [topoged/gedcom "0.1.0"]
+                              
                               [marginalia "0.8.0"] 
                               [org.clojure/test.check "0.5.9"] ;; property testing
                               ]}}
